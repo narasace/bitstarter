5201aa0f4de92b952d1d501bdb90ebb71687f635
Ian <sya9@sfu.ca>
2013-08-24 04:43:07 +0000

    web.js modified to take html file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var bar = new Buffer(100);
 app.use(express.logger());
 
+bar = fs.readFileSync(index.html);
+
 app.get('/', function(request, response) {
-  response.send('Hello Bill! How are you? This is cool, right?');
+  response.send(bar.toString);
 });
 
 var port = process.env.PORT || 5000;
